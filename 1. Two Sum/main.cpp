c60301e086b5d83bd9d1f2daa89b61c13f027381
Usama Rashid Butt <usamarashid.work@gmail.com>
2023-08-29 19:10:48 +0500

    [Sol]: 1. Two Sum

---
+++ 1. Two Sum/main.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target)  {
+        int length = nums.size();
+        vector<int> ans;
+        for(int i=0;i<length;i++){
+            for(int j=i+1;j<length;j++){
+                if(nums[i]+nums[j]==target){
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    
+                    return ans;    
+                }               
+            }
+        }
+        return ans;
+    }
+};
