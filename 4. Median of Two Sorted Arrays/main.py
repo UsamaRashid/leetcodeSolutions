f1dea1d207f155ffc20cc8ed2601d42b97df58ff
Usama Rashid Butt <usamarashid.work@gmail.com>
2023-09-14 16:57:58 +0500

    Optimised Code

--- 4. Median of Two Sorted Arrays/main.py
+++ 4. Median of Two Sorted Arrays/main.py
@@ -120,34 +120,167 @@
 #         #             i = i + 1
 #         # elif i == m - 1:
 #         #     j = j + 1
+## problem when submitting this sol
+
+# class Solution:
+#     def findMedianSortedArrays(self, nums1: [int], nums2: [int]) -> float:
+#         p1 = 0
+#         p2 = 0
+#         new = []
+#         while p1 < len(nums1) and p2 < len(nums2):
+#             if nums1[p1] < nums2[p2]:
+#                 new.append(nums1[p1])
+#                 p1 += 1
+#             else:
+#                 new.append(nums2[p2])
+#                 p2 += 1
+#         while p1 < len(nums1):
+#             new.append(nums1[p1])
+#             p1 += 1
+#         while p2 < len(nums2):
+#             new.append(nums2[p2])
+#             p2 += 1
+#         if len(new) % 2 == 0:
+#             index = len(new) // 2
+#             median = (new[index] + new[index - 1]) / 2
+#         else:
+#             median = float(new[len(new) // 2])
+#         return median
 
 
-class Solution:
-    def findMedianSortedArrays(self, nums1: [int], nums2: [int]) -> float:
-        p1 = 0
-        p2 = 0
-        new = []
-        while p1 < len(nums1) and p2 < len(nums2):
-            if nums1[p1] < nums2[p2]:
-                new.append(nums1[p1])
-                p1 += 1
-            else:
-                new.append(nums2[p2])
-                p2 += 1
-        while p1 < len(nums1):
-            new.append(nums1[p1])
-            p1 += 1
-        while p2 < len(nums2):
-            new.append(nums2[p2])
-            p2 += 1
-        if len(new) % 2 == 0:
-            index = len(new) // 2
-            median = (new[index] + new[index - 1]) / 2
+# class Solution:
+#     def findMedianSortedArrays(self, nums1: [int], nums2: [int]) -> float:
+#         m = len(nums1)
+#         n = len(nums2)
+#         total = m + n
+#         evenMiddle = False
+#         middleNum = total // 2
+#         temp = 0
+#         if total % 2 == 0:
+#             evenMiddle = True
+
+#         i = 0
+#         j = 0
+# index = 0
+# while index < total:
+#     # print(
+#     #     "Index: ",
+#     #     index,
+#     #     " i:",
+#     #     i,
+#     #     " j:",
+#     #     j,
+#     #     " nums1[i]",
+#     #     nums1[i],
+#     #     " nums2[j]",
+#     #     nums2[j],
+#     # )
+#     # if nums1[i] < nums2[j]:
+#     #     if index == middleNum:
+#     #         if evenMiddle:
+#     #             print("nums1[i], temp", nums1[i], temp)
+#     #             return (nums1[i] + temp) / 2
+#     #         else:
+#     #             print("nums1[i]: ", nums1[i])
+#     #             return nums1[i]
+
+#     #     # temp = nums1[i]
+#     #     if evenMiddle and (i != m - 1):
+#     #         temp = nums1[i]
+#     #     else:
+#     #         temp = nums2[j]
+
+#     #     if i < m - 1:
+#     #         i = i + 1
+
+#     # elif nums1[i] > nums2[j]:
+#     #     if index == middleNum:
+#     #         if evenMiddle:
+#     #             print("nums2[j], temp", nums2[j], temp)
+#     #             return (nums2[j] + temp) / 2
+#     #         else:
+#     #             print("nums2[j]: ", nums2[j])
+
+#     #             return nums2[j]
+
+#     #     temp = nums2[j]
+#     #     if evenMiddle and (j != n - 1):
+#     #         temp = nums2[i]
+#     #     else:
+#     #         temp = nums1[j]
+
+#     #     if j < n - 1:
+#     #         j = j + 1
+
+#     # index = index + 1
+
+# while index < total:
+#     print("nums1[i], nums2[j]", nums1[i], nums2[j])
+#     if nums1[i] < nums2[j]:
+#         temp = nums1[i]
+#         if i < m - 1:
+#             i += 1
+#         elif i == m - 1:
+#             if j < n - 1:
+#                 j += 1
+#     elif nums2[j] < nums1[i]:
+#         temp = nums2[j]
+#         if j < n - 1:
+#             j += 1
+#         elif j == n - 1:
+#             if i < m - 1:
+#                 i += 1
+
+#     print("index", index, "temp", temp)
+
+#     index += 1
+
+
+# nums1 = [
+#     1,
+# ]
+# nums2 = [2, 4]
+# print(Solution().findMedianSortedArrays(nums1, nums2))
+
+
+def findMedianSortedArrays(self, nums1: [int], nums2: [int]) -> float:
+    m = len(nums1)
+    n = len(nums2)
+    total = m + n
+    evenMiddle = False
+    middleNum = total // 2
+    temp = 0
+    if total % 2 == 0:
+        evenMiddle = True
+
+    i = 0
+    j = 0
+    index = 0
+    prev_temp = 0  # Added to keep track of the previous temp value
+
+    while index < total:
+        if i < m and (j >= n or nums1[i] < nums2[j]):
+            temp = nums1[i]
+            i += 1
         else:
-            median = float(new[len(new) // 2])
-        return median
+            temp = nums2[j]
+            j += 1
+
+        if evenMiddle and index == middleNum - 1:
+            prev_temp = temp
+        elif index == middleNum:
+            if evenMiddle:
+                return (prev_temp + temp) / 2.0
+            else:
+                return float(temp)
+
+        index += 1
+
+    return 0.0  # This line is added to handle the case when both input arrays are empty
 
 
+# Example usage:
 nums1 = [1, 2]
 nums2 = [3, 4]
-print(Solution().findMedianSortedArrays(nums1, nums2))
+median = findMedianSortedArrays(None, nums1, nums2)
+print(median)
